561743de516805e8bbefff32caf2a9326718ca05
Egor <geksut@gmail.com>
2018-01-21 23:46:18 +0700

    hey

--- cryptoproject/settings.py
+++ cryptoproject/settings.py
@@ -120,8 +120,12 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
-STATIC_URL='/static/'
+# Extra places for collectstatic to find static files.
+
+
 
 
 from datetime import timedelta
