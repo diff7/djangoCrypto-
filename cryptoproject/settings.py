40fa223c06f89276472de32c7e7ca490e122f92c
Egor <geksut@gmail.com>
2018-02-01 00:32:10 +0700

    test

--- cryptoproject/settings.py
+++ cryptoproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y0z0j))mk9h4c=zl$*r)4-yu@i!lfpkz9o_rkpfzuhtc4*6me9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['rabbitrun.herokuapp.com','127.0.0.1']
 
